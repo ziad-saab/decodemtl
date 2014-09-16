1dc56d06f41b1c03ecb570a35c636cfbdb3992e1
Ziad Saab <ziad.saab@gmail.com>
2014-09-16 19:16:24 -0400

    intro section done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,13 @@
 var $ = require('jquery');
 var FastClick = require('fastclick');
 require('foundation/foundation');
+require('hw-parallax/src/hw-parallax');
 
 FastClick.attach(document.body);
 $(document).foundation();
+
+if (!Modernizr.touch) {
+  $('.intro-section').parallax({
+    scroll_factor: 0.5
+  });
+}
