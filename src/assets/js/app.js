f355fb66edc5a7356d03e421fed027bb318be485
Ziad Saab <ziad.saab@gmail.com>
2014-09-18 13:29:27 -0400

    added smooth scrolling of links

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,6 +6,7 @@ require('hw-parallax/src/hw-parallax');
 FastClick.attach(document.body);
 $(document).foundation();
 
+// Parallax
 if (!Modernizr.touch) {
   $('.intro-section,.schedule').parallax({
     scroll_factor: 0.5
@@ -14,3 +15,18 @@ if (!Modernizr.touch) {
     scroll_factor: 0.2
   });
 }
+
+// Smooth scrolling of anchors
+var scrollSpeed = 1.66; // pixels per millisecond
+var $htmlBody = $('html,body');
+var navbarHeight = $('.main-header').outerHeight();
+$(document.body).on('click', 'a[href^=#]', function() {
+  var $target = $(this.hash);
+  if (!$target) {
+    return;
+  }
+  var currentScrollTop = $htmlBody.scrollTop();
+  var targetScrollTop = $target.offset().top - navbarHeight;
+  var mills = Math.abs((targetScrollTop - currentScrollTop) / scrollSpeed);
+  $htmlBody.animate({scrollTop: targetScrollTop}, mills);
+});
