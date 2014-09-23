6a2eaf12365e401fff2b1eb639170cefedbc0969
Ziad Saab <ziad.saab@gmail.com>
2014-09-23 11:30:46 -0400

    fix accordion messing with parallax

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -21,7 +21,8 @@ if (!Modernizr.touch) {
 var scrollSpeed = 1.66; // pixels per millisecond
 var $htmlBody = $('html,body');
 var navbarHeight = $('.main-header').outerHeight();
-$(document.body).on('click', 'a[href^=#]', function() {
+var $body = $(document.body);
+$body.on('click', 'a[href^=#]', function() {
   var $target = $(this.hash);
   if (!$target.length) {
     return;
@@ -30,6 +31,10 @@ $(document.body).on('click', 'a[href^=#]', function() {
   var targetScrollTop = $target.offset().top - navbarHeight;
   var mills = Math.abs((targetScrollTop - currentScrollTop) / scrollSpeed);
   $htmlBody.animate({scrollTop: targetScrollTop}, mills);
+});
+
+$body.on('click', '.course-curriculum__header', function() {
+  setTimeout(function() {$(window).trigger('hwparallax.reconfigure');}, 500);
 });
 
 // Application form
