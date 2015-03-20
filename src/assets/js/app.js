84aeaf7b8e160d5f773174cb7f00c47157e44228
Ziad Saab <ziad.saab@gmail.com>
2015-03-20 09:22:10 -0400

    New site design

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -17,29 +17,9 @@ if (!Modernizr.touch) {
   });
 }
 
-// Smooth scrolling of anchors
-var scrollSpeed = 1.66; // pixels per millisecond
-var $htmlBody = $('html,body');
-var navbarHeight = $('.main-header').outerHeight();
 var $body = $(document.body);
-$body.on('click', 'a[href^=#]', function() {
-  var $target = $(this.hash);
-  if (!$target.length) {
-    return;
-  }
-  var currentScrollTop = $htmlBody.scrollTop();
-  var targetScrollTop = $target.offset().top - navbarHeight;
-  var mills = Math.abs((targetScrollTop - currentScrollTop) / scrollSpeed);
-  $htmlBody.animate({scrollTop: targetScrollTop}, mills);
-  
-  // Send fake pageview to GA when clicking on anchor
-  window.ga && ga('send', 'pageview', '/' + this.hash.substr(1));
-});
-
 $body.on('click', '.course-curriculum__header', function() {
-  // re-draw parallax because page height has changed
-  setTimeout(function() {$(window).trigger('hwparallax.reconfigure');}, 500);
-  
+
   // since the accordion is "css-only", this enables clearing the radio button if current box is open
   var $for = $('#' + $(this).attr('for'));
   if ($for.length && $for.prop('checked')) {
@@ -50,12 +30,12 @@ $body.on('click', '.course-curriculum__header', function() {
     var unitName = $(this).find('.course-curriculum__unit-name').text();
     window.ga && ga('send', 'event', 'curriculum', 'expand unit', unitName);
   }
+
+  // re-draw parallax because page height has changed
+  setTimeout(function() {$(window).trigger('hwparallax.reconfigure');}, 0);
 });
 
 $body.on('click', '.faq-list__header', function() {
-  // re-draw parallax because page height has changed
-  setTimeout(function() {$(window).trigger('hwparallax.reconfigure');}, 500);
-
   // since the accordion is "css-only", this enables clearing the radio button if current box is open
   var $for = $('#' + $(this).attr('for'));
   if ($for.length && $for.prop('checked')) {
@@ -66,6 +46,9 @@ $body.on('click', '.faq-list__header', function() {
     var questionText = $(this).find('.faq-list__header').text();
     window.ga && ga('send', 'event', 'faq', 'expand question', questionText);
   }
+
+  // re-draw parallax because page height has changed
+  setTimeout(function() {$(window).trigger('hwparallax.reconfigure');}, 0);
 });
 
 // Application form
