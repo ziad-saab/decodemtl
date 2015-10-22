5b0e3b0f3bafe9141a81de0a1442294ee03dfe95
Ziad Saab <ziad.saab@gmail.com>
2015-10-22 08:05:57 -0400

    Updates

--- assets/js/modernizr.js
+++
@@ -1,4 +0,0 @@
-/* Modernizr (Custom Build) | MIT & BSD
- * Build: http://modernizr.com/download/#-prefixed-shiv-load-cssclasses-touch-mq-opacity-input-contenteditable-cors-geolocation-history-svg-csstransforms-json
- */
-;Modernizr.addTest("json",!!window.JSON&&!!JSON.parse),Modernizr.addTest("cors",!!(window.XMLHttpRequest&&"withCredentials"in new XMLHttpRequest)),Modernizr.addTest("contenteditable","contentEditable"in document.documentElement);
